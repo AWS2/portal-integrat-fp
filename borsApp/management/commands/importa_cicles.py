a01aac6c5ea12c764bd93e564e52e4016d422154
Enric <emieza@xtec.cat>
2019-03-19 18:02:38 +0100

    importació cicles

--- borsApp/management/commands/importa_cicles.py
+++ borsApp/management/commands/importa_cicles.py
@@ -6,7 +6,18 @@ import csv
 class Command(BaseCommand):
     help = 'Carrega dades dels cicles formatius'
 
+    cicles = {}
+
     def handle(self, *args, **options):
-    	print("hola")
-    	"""with open('misc/Taules_cataleg_FP_18-19-LOE.csv') as csvfile:
-    		csv_reader = csvfile"""
+        cicles = {}
+        with open('misc/Taules_cataleg_FP_18-19-LOE.csv') as csvfile:
+            csv_reader = csv.DictReader( csvfile )
+            for row in csv_reader:
+                self.cicles[row["CODI_CICLE_FORMATIU"]] = row["NOM_CICLE_FORMATIU"]
+        # mostrem cicles
+        self.show()
+
+    def show(self):
+        for codi in self.cicles:
+            print(self.cicles[codi])
+
