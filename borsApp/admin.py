e752a6c6400d9f867b726a54c1187710fa8ead6e
Enric <emieza@xtec.cat>
2019-03-20 19:10:04 +0100

    new fixtures cats i cicles

--- borsApp/admin.py
+++ borsApp/admin.py
@@ -10,7 +10,12 @@ class CatAdmin(admin.ModelAdmin):
 	model = Categoria
 	list_display = ('nom','pare','descripcio')
 	search_fields = ('nom','pare__nom')
+	ordering = ('pare','nom',)
 
+class CicleAdmin(admin.ModelAdmin):
+	list_display = ('nom','familia')
+	ordering = ('familia','nom',)
 
 admin.site.register( Categoria, CatAdmin )
-admin.site.register( Cicle )
+admin.site.register( Cicle, CicleAdmin )
+
