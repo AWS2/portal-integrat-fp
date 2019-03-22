6b3cae50c0ff6bb9129821b777e35a9df5fc5ff1
Enric <emieza@xtec.cat>
2019-03-22 20:34:08 +0100

    mes

--- borsApp/management/commands/importa_centres.py
+++ borsApp/management/commands/importa_centres.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from borsApp.models import Cicle, Categoria
+from borsApp.models import Cicle, Categoria, Centre
+from django.contrib.gis.geos import Point
 
 import csv
 
@@ -10,4 +11,21 @@ class Command(BaseCommand):
         with open('misc/Directori_de_centres_docents.csv') as csvfile:
             csv_reader = csv.DictReader( csvfile )
             for row in csv_reader:
+                centre = Centre()
+                centre.codi = row["Codi centre"]
+                centre.nom = row["Denominació completa"]
+                centre.direccio = row["Adreça"]
+                centre.poblacio = row["Nom localitat"]
+                centre.cp = row["Codi postal"]
+                centre.telefon = 1 #row["Telèfon"]
+                centre.email = row["E-mail centre"]
+                centre.web = ""
+                x = row["Coordenades GEO X"]
+                y = row["Coordenades GEO Y"]
+                if( x and y ):
+                    centre.localitzacio = Point(float(x),float(y))
+                else:
+                    centre.localitzacio = Point(0,0)
+                centre.save()
                 print(row["Codi centre"]+" | "+row["Denominació completa"])
+
