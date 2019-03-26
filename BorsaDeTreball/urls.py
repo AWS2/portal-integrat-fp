af2d65305096e29d3c267c128f229f7ed7070626
GODSKADI <aidrean.gf@gmail.com>
2019-03-26 20:30:42 +0100

    style.css

--- BorsaDeTreball/urls.py
+++ BorsaDeTreball/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('borsApp.urls')),
+    path('login', include('borsApp.urls')),
 
 ]
