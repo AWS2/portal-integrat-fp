1a66db0e8d58e899de20cb5ef6fe0762985901e6
RMugetsu <43476238+RMugetsu@users.noreply.github.com>
2019-04-01 03:40:09 +0200

    Okey

--- BorsaDeTreball/settings2.py
+++ BorsaDeTreball/settings2.py
@@ -1,6 +1,6 @@
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "aws2borsapp@gmail.com"
-EMAIL_HOST_PASSWORD = "adminAws2"
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST = ""
+EMAIL_PORT = 
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = 
+EMAIL_USE_SSL = 
