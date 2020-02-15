9750c3c54387c212a8cac0b1f4e5e4d46c44742d
Enric Mieza <emieza@xtec.cat>
2020-02-15 22:50:11 +0100

    visualització projectes

--- scrum/views.py
+++ scrum/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
+from scrum.models import Projecte
+
 # Create your views here.
+
+
+def index(request):
+	projectes = Projecte.objects.all()
+	return render( request, "projecte_list.html", {"projectes":projectes} )
+
+def projecte(request,id):
+	projecte = Projecte.objects.get(id=id)
+	return render( request, "projecte.html", {"projecte":projecte} )
+
