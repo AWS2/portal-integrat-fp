19b32be06a0f9c4e37b137d9565b2e7e214ec843
Enric <emieza@xtec.cat>
2021-10-25 14:03:40 +0200

    object serializers and requirements

--- scrum/urls.py
+++ scrum/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path( 'projecte', views.index, name='projectes' ),
     path( 'projectes', views.index, name='projectes2' ),
     path( 'projecte/<int:id>', views.projecte, name="projecte" ),
-    path( 'api/actualitza_qualificacions', lib.api_actualitza_qualificacions, name="actualitza_qualificacions" ),
     path( 'qualifica', views.qualifica ),
+
+    # api
+    path( 'api/actualitza_qualificacions', lib.api_actualitza_qualificacions,
+                                    name="actualitza_qualificacions" ),
+    path( 'api/get_equips', lib.api_get_equips, name="get_equips" ),
 ]
 
